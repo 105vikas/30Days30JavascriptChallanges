36a02e34fa6678187bd0f667da38e6ccf2598e4e
Vikas Yadav <sonu@Vikass-MacBook-Air.local>
2026-07-02 00:58:18 +0530

    FAQ Accordian changes

--- FAQAccordion/script.js
+++ FAQAccordion/script.js
@@ -1,7 +1,10 @@
 let questionList=document.querySelectorAll('.questionList');
 
 questionList.forEach(ques=>{
-    ques.addEventListener('click',()=>{
+    ques.addEventListener('click',(e)=>{
+        console.log(ques,e.target.children[1])
+        e.target.classList.toggle('active')
+        e.target.children[1].classList.toggle('active')
         ques.classList.toggle('active')
     })
 })
